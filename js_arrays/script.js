621080505bee3283d00f2b7579bce95c444e01ac
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-08-13 15:03:32 +0530

    thats enough for today

--- js_arrays/script.js
+++ js_arrays/script.js
@@ -1,5 +1,11 @@
+//map filter reduce(MFR)
+
+
+//1.MAP
+
 let arr = [1, 13, 5 ,7, 11];
 let newArr = []
+
 for (let index = 0; index < arr.length; index++) {
     const element = arr[index];
     newArr.push(element**2)
@@ -9,6 +15,8 @@ let newArr = arr.map((e, index, array)=>{
     return e**2
 })
 
+//2.FILTER
+
 console.log(newArr)
 const greaterThanSeven = (e)=>{
     if(e>7){
@@ -16,6 +24,9 @@ const greaterThanSeven = (e)=>{
     }
     return false
 }
+
+//3. REDUCE
+
 console.log(arr.filter(greaterThanSeven))
 
 let arr2 = [1,2,3,4,5,6]
@@ -24,4 +35,4 @@ const red = (a, b)=>{
     return a+b
 }
 
-console.log(arr2.reduce(red))
+console.log(arr2.reduce(red))// yaha red ki jagah kuch bhi likh sakte hai compulsaro nhi hai just main thing yeh hai ki konsa operation apply ho raha hi red is just a name that comes after const
