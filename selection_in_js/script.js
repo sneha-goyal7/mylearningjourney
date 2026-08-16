7dfaa9a78e4b869c706f516b88d46239a79564e5
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-08-16 12:57:15 +0530

    array in js

--- selection_in_js/script.js
+++ selection_in_js/script.js
@@ -1,15 +1,18 @@
 console.log("Harry")
 
-// let boxes = document.getElementsByClassName("box")
-// console.log(boxes)
+let boxes = document.getElementsByClassName("box")
+console.log(boxes)
 
-// boxes[2].style.backgroundColor = "red"
+boxes[2].style.backgroundColor = "red"
 
-// document.getElementById("redbox").style.backgroundColor = "red"
+document.getElementById("redbox").style.backgroundColor = "red"
 
-// document.querySelector(".box").style.backgroundColor = "green";
+document.querySelector(".box").style.backgroundColor = "green";
 console.log(document.querySelectorAll(".box"))
 
-document.querySelectorAll(".box").forEach(e =>{
-    e.style.backgroundColor = "green";
-}) 
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor = "green";
+// }) 
+
+//when u have to choose or apply css for the particular box then there is no need to use loop
+//but when it apply on all boxes etc then use loop.
