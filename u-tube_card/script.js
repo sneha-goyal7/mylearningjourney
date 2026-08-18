b755657a18f08eca71e36b887a222bd94de09844
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-08-18 08:46:01 +0530

    new day

--- u-tube_card/script.js
+++ u-tube_card/script.js
@@ -1,7 +1,7 @@
 function createCard(title, cName, views, monthsOld, duration, thumbnail) {
 
     // Finish this function
-    
+
     let viewStr
     if (views < 1000) {
         viewStr = views;
@@ -27,7 +27,6 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail) {
 
     document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 }
-
 
 createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
 createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
