def7925a43f5b1648f4571f2e3addab0399336f2
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-08-23 12:36:23 +0530

    fetching from website

--- fetch_api/script.js
+++ fetch_api/script.js
@@ -13,7 +13,10 @@
 
 async function getData() {
     // Simulate getting data from a server
-    // let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+   
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //   .then(response => response.json())
+    //   .then(json => console.log(json))
 
     let x = await fetch('https://jsonplaceholder.typicode.com/posts', {
                 method: 'POST',
